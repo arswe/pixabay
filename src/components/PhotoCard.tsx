0358c1698963a7e30382343e397a6d45bec47234
arswe <abdurrahmansoftw@gmail.com>
2024-02-21 22:53:17 +0600

    navabr update

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -1,13 +1,23 @@
-import { ImageListItem } from '@mui/material'
+import InfoIcon from '@mui/icons-material/Info'
+import { IconButton, ImageListItem, ImageListItemBar } from '@mui/material'
 
 const PhotoCard = ({ photo }) => {
   return (
-    <ImageListItem>
+    <ImageListItem key={photo.img}>
       <img
-        srcSet={`${photo.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
-        src={`${photo.img}?w=161&fit=crop&auto=format`}
+        srcSet={`${photo.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+        src={`${photo.img}?w=248&fit=crop&auto=format`}
         alt={photo.title}
         loading='lazy'
+      />
+      <ImageListItemBar
+        title={photo.title}
+        subtitle={photo.author}
+        actionIcon={
+          <IconButton sx={{ color: 'rgba(255, 255, 255, 0.54)' }} aria-label={`info about ${photo.title}`}>
+            <InfoIcon />
+          </IconButton>
+        }
       />
     </ImageListItem>
   )
--- src/layouts/PhotoGrid.tsx
+++ src/layouts/PhotoGrid.tsx
@@ -2,7 +2,7 @@ import { Container, Grid, ImageList } from '@mui/material'
 import PhotoCard from '../components/PhotoCard'
 
 const PhotoGrid = () => {
-  const photos = [1, 2, 3]
+  const photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
   return (
     <Container
       sx={{
@@ -17,7 +17,7 @@ const PhotoGrid = () => {
     >
       <Grid container spacing={2}>
         {photos.map((photo, index) => (
-          <Grid item xs={4} key={index}>
+          <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={index}>
             <ImageList variant='woven'>
               <PhotoCard photo={photo} />
             </ImageList>
