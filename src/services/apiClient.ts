8304f58d4c5b1af1000eadf8f0447e56a978a777
arswe <abdurrahmansoftw@gmail.com>
2024-02-21 22:54:47 +0600

    add api key

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,3 +1,8 @@
-const apiClient = () => {}
+import axios from 'axios'
 
-export default apiClient
+export default axios.create({
+  baseURL: 'https://pixabay.com/api/',
+  params: {
+    api_key: '42493300-555c9a42a17d9b717de5e9c96',
+  },
+})
